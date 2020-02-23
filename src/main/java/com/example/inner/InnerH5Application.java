0e2c1a06aeb369c194d997b1f28913ba4e8cb909
jxd.dengpeng <123456>
2020-02-23 10:50:36 +0800

    第一次提交

---
+++ src/main/java/com/example/inner/InnerH5Application.java
@@ -0,0 +1,13 @@
+package com.example.inner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnerH5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnerH5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inner/InnerH5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnerH5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
